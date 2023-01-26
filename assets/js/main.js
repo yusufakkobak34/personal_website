d1a3f442ce7f302f1a4b06f448134edc3dd10dbb
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-01-27 02:55:18 +0300

    skill section completed succesfully.

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,24 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillContent = document.getElementsByClassName('skills__content'),
+      skillHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillContent.length; i++){
+        skillContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+      
 
 /*==================== QUALIFICATION TABS ====================*/
 
