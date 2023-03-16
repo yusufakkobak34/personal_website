b47c5a10b13487c8515d70f08bf0d6a5f7401393
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-03-17 02:31:43 +0300

    section edited and some image changes.

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const iconToggle = document.querySelector('.toggle_icon');
+const navbarMenu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu_link');
+const iconClose = document.querySelector('.close_icon');
+
+iconToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('active');
+});
+
+iconClose.addEventListener('click', () => {
+    navbarMenu.classList.remove('active');
+})
+
+menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener('click', () => {
+      navbarMenu.classList.remove('active');
+    });
+});
+
+function scrollHeader(){
+  const header = document.getElementById('header');
+  this.scrollY >= 20 ? header.classList.add('active') : header.classList.remove('active');
+}
+
+window.addEventListener('scroll', scrollHeader);
