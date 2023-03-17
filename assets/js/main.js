5aab21f87c36735572fbe9e80ec0193351632207
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-03-17 03:00:42 +0300

    about section completed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,17 @@ function scrollHeader(){
 }
 
 window.addEventListener('scroll', scrollHeader);
+
+const typed = document.querySelector('.typed');
+
+if(typed){
+  let typed_strings = typed.getAttribute('data-typed-items');
+  typed_strings = typed_strings.split(',');
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  });
+}
