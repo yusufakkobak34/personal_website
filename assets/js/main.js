f1be87378fc2c3bcadac4c4eba8a0e55e23e032a
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-04-30 03:37:08 +0300

    js animation code added.

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,9 +23,15 @@ window.onscroll = () => {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
+            });
+            // active sections for animation on scroll 
+            sec.classList.add('show-animate');
         }
-    })
+        // if want to use animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
+        }
+    });
 
     //sticky header
     let header = document.querySelector('header');
