010e06c65e241f8c4d9bfa3e3bcb668c8eb27e4d
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-05-02 00:26:30 +0300

    all coding section's fully completed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,4 +40,8 @@ window.onscroll = () => {
     // remove toggle icon and navbar when click navbar links(scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+    
+    //animation footer on scroll
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
